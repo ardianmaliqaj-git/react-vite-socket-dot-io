652edf29f22265049519d85759b2bace786d7b2f
Arian Maliqaj <ardianmaliqaj49@gmail.com>
2022-06-29 20:22:51 +0200

    automatic build for socket-dot-io server

--- README.md
+++
@@ -1,5 +0,0 @@
-git status
-git add .
-git status
-git commit --amend --no-edit
-git push origin master --force
--- webpack.config.js
+++ webpack.config.js
@@ -1,47 +1,48 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-const prompt = require("prompt-sync")();
-let folder = prompt("Any specific folder?: ");
+console.log(path.resolve(__dirname, "socket-dot-io", "public"));
 
-module.exports = {
-  devtool: "inline-source-map",
-  entry: {
-    path: path.resolve(__dirname, "main.js"),
-  },
-  output: {
-    clean: true,
-    filename: "main.[contenthash].js",
-    path: path.resolve(__dirname, folder || "public"),
-  },
-  plugins: [
-    new HtmlWebpackPlugin({ template: "main.html", favicon: "favicon.ico" }),
-  ],
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node-moudles/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: [
-              "@babel/preset-env",
-              ["@babel/preset-react", { runtime: "automatic" }],
-            ],
+module.exports = (_, argv) => {
+  return {
+    devtool: "inline-source-map",
+    entry: {
+      path: path.resolve(__dirname, "main.js"),
+    },
+    output: {
+      clean: true,
+      filename: "main.[contenthash].js",
+      path: path.resolve(__dirname, "socket-dot-io", "public"),
+    },
+    plugins: [
+      new HtmlWebpackPlugin({ template: "main.html", favicon: "favicon.ico" }),
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node-moudles/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: [
+                "@babel/preset-env",
+                ["@babel/preset-react", { runtime: "automatic" }],
+              ],
+            },
           },
         },
-      },
-      {
-        test: /\.css$/,
-        exclude: /node-moudles/,
-        use: ["style-loader", "css-loader"],
-      },
-    ],
-  },
-  devServer: {
-    port: process.env.PORT || 3000,
-    hot: true,
-    open: true,
-  },
+        {
+          test: /\.css$/,
+          exclude: /node-moudles/,
+          use: ["style-loader", "css-loader"],
+        },
+      ],
+    },
+    devServer: {
+      port: process.env.PORT || 3000,
+      hot: true,
+      open: true,
+    },
+  };
 };
