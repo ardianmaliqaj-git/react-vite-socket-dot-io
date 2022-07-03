3e61a029fec1faf63ff830fd6b1df552bc33a1a1
Arian Maliqaj <ardianmaliqaj49@gmail.com>
2022-07-03 19:17:19 +0200

    changing vvite.config.js specifications format

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,18 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  build: { outDir: "./server/static/", assetsDir: "./files/", sourcemap: true },
   plugins: [react()],
+  build: {
+    outDir: "./server/static/",
+    sourcemap: true,
+    emptyOutDir: true,
+    minify: "esbuild",
+    rollupOptions: {
+      output: {
+        entryFileNames: "[name].[hash].js",
+        chunkFileNames: "[name].[hash].js",
+        assetFileNames: "[name].[hash].[ext]",
+      },
+    },
+  },
 });
