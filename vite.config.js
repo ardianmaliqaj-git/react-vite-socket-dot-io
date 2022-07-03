9245802c9ac48c70bcae409b0fe89801ece4e19d
Arian Maliqaj <ardianmaliqaj49@gmail.com>
2022-07-03 23:42:13 +0200

    fixing broken source maps with --no-border-checks

--- vite.config.js
+++ vite.config.js
@@ -1,20 +1,21 @@
 import { defineConfig } from "vite";
-
 import react from "@vitejs/plugin-react";
 
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: "./server/static/",
-    sourcemap: true,
-    emptyOutDir: true,
-    minify: "esbuild",
-    rollupOptions: {
-      output: {
-        entryFileNames: "[name].[hash].js",
-        chunkFileNames: "[name].[hash].js",
-        assetFileNames: "[name].[hash].[ext]",
+export default defineConfig((env) => {
+  return {
+    plugins: [react()],
+    build: {
+      outDir: "./server/static/",
+      sourcemap: env.mode == "production" ? false : true,
+      emptyOutDir: true,
+      minify: true,
+      rollupOptions: {
+        output: {
+          entryFileNames: "[name].[hash].js",
+          chunkFileNames: "[name].[hash].js",
+          assetFileNames: "[name].[hash].[ext]",
+        },
       },
     },
-  },
+  };
 });
