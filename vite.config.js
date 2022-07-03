b8e33f8559767d88bbc98760344ce7d23c4db904
Arian Maliqaj <ardianmaliqaj49@gmail.com>
2022-07-03 18:49:01 +0200

    enable source map for source map explorer

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  build: { outDir: "./server/static/", assetsDir: "./files/" },
+  build: { outDir: "./server/static/", assetsDir: "./files/", sourcemap: true },
   plugins: [react()],
 });
